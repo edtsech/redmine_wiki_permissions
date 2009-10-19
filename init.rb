d42e0948548f04637e0111976a12fbbf76c75ba8
Edward Tsech <edtsech@gmail.com>
2009-10-20 00:06:01 +0700

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_wiki_permissions do
+  name 'Redmine Wiki Permissions plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
