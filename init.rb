9ee79d92a879f56b013db60f956f2f4e340c02ab
Edward Tsech <edtsech@gmail.com>
2010-03-21 23:07:49 +0600

    Added description for plugin

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ require File.dirname(__FILE__) + '/lib/wiki_permissions.rb'
 Redmine::Plugin.register :redmine_wiki_permissions do
   name 'Redmine Wiki Permissions plugin'
   author 'Equelli'
-  description 'This is a plugin for Redmine'
+  description 'This redmine plugin adding permissions for every wiki page'
   version '0.0.1'
   
   project_module "wiki_permissions" do
